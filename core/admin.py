e65749c69666131db5f9979914b66efb47bfbde3
iamkotik <kayasmolensk@gmail.com>
2022-10-25 14:53:50 +0300

    test redeploy to pa with db

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from core.models import *
+
 
 # Register your models here.
+@admin.register(Vaccinations)
+class VaccinationsAdmin(admin.ModelAdmin):
+    list_display = ("code", "name")
--- core/models.py
+++ core/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+RISK = (
+    ('YES', 'Да'),
+    ('NO', 'Нет'),
+)
+
+TYPES = (
+    ('KIDS', 'Дети'),
+    ('ADULTS', 'Взрослые'),
+)
+
+
+class Vaccinations(models.Model):
+    code = models.CharField(max_length=10, primary_key=True)
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return "{0}".format(self.name)
+
+    class Meta:
+        verbose_name = "Прививка"
+        verbose_name_plural = "Прививки"
